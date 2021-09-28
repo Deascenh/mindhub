75a72143a78f026729a6fcccd0c862a651d3b212
ThibaultM <t.moulin76@gmail.com>
2021-09-28 15:14:18 +0200

    Put base project

---
+++ api-mindhub/api/src/Entity/Greeting.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * This is a dummy entity. Remove it!
+ *
+ * @ORM\Entity
+ */
+#[ApiResource(
+    collectionOperations: ['get', 'post'],
+    itemOperations: ['get', 'put', 'delete'],
+    mercure: true,
+)]
+class Greeting
+{
+    /**
+     * The entity ID
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id = null;
+
+    /**
+     * A nice person
+     *
+     * @ORM\Column
+     */
+    #[Assert\NotBlank]
+    public string $name = '';
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
